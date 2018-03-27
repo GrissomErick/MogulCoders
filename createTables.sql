ab12741a0adc4047a187a8a9c01c47deb9216f8e
Erick Grissom <31044352+GrissomErick@users.noreply.github.com>
2018-03-27 18:09:34 -0400

    Initial app & server files

---
+++ createTables.sql
@@ -0,0 +1,6 @@
+/* Creates blank database for Project #2 - Critter Sitter*/
+
+DROP DATABASE IF EXISTS critter_users; 
+
+CREATE DATABASE critter_users;
+
---
+++ server.js
@@ -0,0 +1,64 @@
+    var express    = require('express')
+    var app        = express()
+    var passport   = require('passport')
+    var session    = require('express-session')
+    var bodyParser = require('body-parser')
+    var env        = require('dotenv').load()
+    var exphbs     = require('express-handlebars')
+
+
+
+    //For BodyParser
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+
+     // For Passport
+    app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+
+
+     //For Handlebars
+    app.set('views', './app/views')
+    app.engine('hbs', exphbs({extname: '.hbs'}));
+    app.set('view engine', '.hbs');
+    
+
+    app.get('/', function(req, res){
+	  res.send('Welcome to Critter Sitter Home page');
+	});
+
+
+	//Models
+    var models = require("./app/models");
+
+
+    //Routes
+    var authRoute = require('./app/routes/auth.js')(app,passport);
+
+
+    //load passport strategies
+    require('./app/config/passport/passport.js')(passport,models.user);
+
+
+    //Sync Database
+   	models.sequelize.sync().then(function(){
+    console.log('Critter Sitter user database is connected!')
+
+    }).catch(function(err){
+    console.log(err,"Something went wrong with the Database Update!")
+    });
+
+
+
+	app.listen(5000, function(err){
+		if(!err)
+		console.log("CritterSitter is live"); else console.log(err)
+
+	});
+
+
+
+
+    
