2a3f88a990eaed04936686f7ece5751c36ee383a
Richard Paez <Richardp809@gmail.com>
2018-03-29 20:07:56 -0400

    protected the sitter route

--- app/routes/sitters.js
+++ app/routes/sitters.js
@@ -1,5 +1,12 @@
 var sitterController = require('../controllers/sitterController');
 
 module.exports = function(app, passport){
-    app.get('/sitters', sitterController.getAllSitters );
+    app.get('/sitters', isLoggedIn , sitterController.getAllSitters );
 };
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.status(403).send( { success: false, msg: 'Unauthorized' } );
+}
